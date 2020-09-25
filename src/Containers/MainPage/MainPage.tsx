6a5890347292435e46f92e8ed31056b20ec221c9
Nefito <vkorzhov00@gmail.com>
2020-09-25 17:20:24 +0300

    Make mapStateToProps and mapDispatchToProps

--- src/Containers/MainPage/MainPage.tsx
+++ src/Containers/MainPage/MainPage.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
-import { fetchCharacters, fetchEpisodes, fetchLocations,  } from 'store/actions';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { RootState } from 'src/store/configureStore';
+import {  fetchCharacters, fetchEpisodes, fetchLocations } from 'store/actions';
 
-const Main = () => {
+const mapStateToProps = (state: RootState) => {
+  return {
+    characters: state.characters,
+    locations: state.locations,
+    episodes: state.episodes
+  };
+};
+
+const mapDispatchToProps = (dispatch: any) => ({
+  fetchCharacters: () =>  { dispatch(fetchCharacters()); },
+  fetchLocations: () =>  { dispatch(fetchLocations()); },
+  fetchEpisodes: () =>  { dispatch(fetchEpisodes()); }
+});
+
+const Main = (props: any) => {
+
+  useEffect(() => {
+    props.fetchCharacters();
+    props.fetchLocations();
+    props.fetchEpisodes();
+  }, []);
+
+  const test = props.characters.characters.results;
+
   return (
-    <p>Main</p>
+    <div>
+      <h1>{test ? test[0].name : null }</h1>
+    </div>
   );
 };
-export default Main;
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
