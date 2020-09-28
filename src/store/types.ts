804de155721aa4b638cd4fbd740d6fff2e92cf8e
Nefito <vkorzhov00@gmail.com>
2020-09-28 17:17:13 +0300

    Delete IAction interface

--- src/store/types.ts
+++ src/store/types.ts
@@ -3,16 +3,9 @@ export interface IStoreState {
   episodes: any;
   locations: any;
 }
-
-export interface IAction {
-  type: string;
-  payload?: any;
-}
   
 export interface IStateSlice {
   isLoading: boolean;
   errMess: string | null;
   [propName: string]: any;
 }
-
-export type DispatchAction = (arg: IAction) => (IAction);
