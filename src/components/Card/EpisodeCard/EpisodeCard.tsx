ce1ab313157bd1f2273b7800bdad77ca54112c7b
Nefito <vkorzhov00@gmail.com>
2020-10-08 10:42:13 +0300

    Remove unnecessary divs

--- src/components/Card/EpisodeCard/EpisodeCard.tsx
+++ src/components/Card/EpisodeCard/EpisodeCard.tsx
@@ -20,13 +20,9 @@ export const EpisodeCard: React.FC<IEpisodeCard> = (props) => {
   return (
     <EpisodeCardStyled>
       <CardBodyWrapper>
-        <div>
-          <a href={episode.url} className="card-text-name link-no-style">{episode.name}</a>
-        </div>
-        <div>
-          <CardBodyItem value={episode.episode} title="Episode:" className="card-info" />
-          <CardBodyItem value={episode.air_date} title="Air Date:" className="card-info" />
-        </div>
+        <a href={episode.url} className="card-text-name link-no-style">{episode.name}</a>
+        <CardBodyItem value={episode.episode} title="Episode:" className="card-info" />
+        <CardBodyItem value={episode.air_date} title="Air Date:" className="card-info" />
         <CharacterList characters={episode.characters} className="card-characters" title="Characters" />
         <LinkButton to={`/episodes/${episode.id}/`} href="/">More...</LinkButton>
       </CardBodyWrapper>
